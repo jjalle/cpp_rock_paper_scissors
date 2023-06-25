20a324607b0911b596e3047cba1161431f730f8b
jjalle <jjalleibarra@gmail.com>
2023-06-25 12:12:48 +0200

    Added code coverage

---
+++ run_code_coverage.sh
@@ -0,0 +1,5 @@
+pushd build/game/CMakeFiles/Game.dir/src/
+gcov *.gcno
+popd
+lcov --capture --directory build/game/CMakeFiles/Game.dir/src/ --output-file build/gtest_coverage.info
+genhtml build/gtest_coverage.info --output-directory build/code_coverage
